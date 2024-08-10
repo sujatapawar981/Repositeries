14a051463ebc5e81511083499db25ade7ca3db19
sujatapawar981 <sujata.pawar981@gmail.com>
2024-08-10 22:14:53 +0530

    06-for-assignment01
    06-for-assignment01

---
+++ javascript-programs1/06-for-assignment.js
@@ -0,0 +1,55 @@
+
+console.log("====== Print Numbers from 5 to 10 By Increament 1 =======");
+ for (i =5; i<=15; i++) {
+    console.log(i);
+    
+}
+
+console.log("====== Print Numbers from 50 to 40 By Decreament 1 =======");
+
+for (i=50; i>=40; i--) {
+   console.log(i);
+   
+    
+}
+
+console.log("====== Finding first 15 Odd numbers ======");
+
+for (i=1; i<=30; i=i+2) {
+    console.log(i);
+    
+    
+}
+
+console.log("====== Print First 10 Even Numbers =======");
+
+for (i=2; i<=20; i=i+2) {
+    console.log(i);
+    
+    
+}
+
+console.log("====== Print Table Of 5  =======");
+
+for (i=5; i<=50; i=i+5) {
+
+    console.log(i);
+    
+    
+}
+
+console.log("====== Print Table of 10 =======");
+
+for (i=10; i<=100; i=i+10) {
+    console.log(i);
+    
+    
+}
+
+console.log("====== Print Table 10 in Reverse Order =======");
+    
+for (let i=100; i>=10; i=i-10) {
+    console.log(i);
+    
+    
+}
